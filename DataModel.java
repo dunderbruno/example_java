263dc903b6337267aa2a3c6bb4c6bf4008fde486
Bruno Santos <7022432+dunderbruno@users.noreply.github.com>
2024-09-04 06:33:57 -0300

    Create DataModel.java

---
+++ DataModel.java
@@ -0,0 +1,11 @@
+// DataModel.java
+package com.example.hexagonal.domain.model;
+
+public class DataModel {
+    private String id;
+    private String name;
+    private String value;
+    private String additionalInfo;
+
+    // Construtores, getters e setters
+}
